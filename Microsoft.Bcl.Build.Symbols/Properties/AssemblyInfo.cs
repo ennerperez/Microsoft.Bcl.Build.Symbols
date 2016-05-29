be39837aa1213275c9b000c9c9924d5a6b1f956e
Enner Pérez <ennerperez@gmail.com>
2016-05-29 00:17:18 -0400

    AssemblyInfo

--- Microsoft.Bcl.Build.Symbols/Properties/AssemblyInfo.cs
+++ Microsoft.Bcl.Build.Symbols/Properties/AssemblyInfo.cs
@@ -1,41 +1,24 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+using System.Resources;
 using System.Runtime.InteropServices;
 
-// La información general de un ensamblado se controla mediante el siguiente
-// conjunto de atributos. Cambie estos valores de atributo para modificar la información
-// asociada con un ensamblado.
 [assembly: AssemblyTitle("Microsoft.Bcl.Build.Symbols")]
 [assembly: AssemblyDescription("Provides compile time symbols for .NET Framework.")]
 #if DEBUG
-[assembly: AssemblyConfiguration("Debug")]
+[assembly: AssemblyConfiguration("DEBUG")]
 #else
-[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyConfiguration("RELEASE")]
 #endif
 [assembly: AssemblyCompany("Enner Pérez")]
 [assembly: AssemblyProduct("Microsoft.Bcl.Build.Symbols")]
-[assembly: AssemblyCopyright("Copyright © Enner Pérez 2016")]
-[assembly: AssemblyTrademark("© Enner Pérez")]
-[assembly: AssemblyCulture("en")]
+[assembly: AssemblyCopyright("Copyright © Enner Pérez")]
+[assembly: AssemblyTrademark("Microsoft")]
+[assembly: AssemblyCulture("")]
 
-// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles
-// para los componentes COM.  Si necesita obtener acceso a un tipo de este ensamblado desde
-// COM, establezca el atributo ComVisible en true en este tipo.
 [assembly: ComVisible(false)]
 
-// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
-[assembly: Guid("a619d960-0942-47bc-b908-f940ecbfa1e1")]
+[assembly: Guid("A619D960-0942-47BC-B908-F940ECBFA1E1")]
 
-// La información de versión de un ensamblado consta de los cuatro valores siguientes:
-//
-//      Versión principal
-//      Versión secundaria
-//      Número de compilación
-//      Revisión
-//
-// Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
-// mediante el carácter '*', como se muestra a continuación:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.*")]
-[assembly: AssemblyFileVersion("1.0.0")]
-//[assembly: AssemblyInformationalVersion("1.0.0-master")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
