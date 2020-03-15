c26c721f98d90e6d86e20150a8d7f89009e67ed2
Enner Pérez <ennerperez@gmail.com>
2020-03-15 04:48:36 -0500

    Version Updates

--- src/Microsoft.Bcl.Build.Symbols/Properties/AssemblyInfo.cs
+++ src/Microsoft.Bcl.Build.Symbols/Properties/AssemblyInfo.cs
@@ -13,6 +13,8 @@ using System.Runtime.InteropServices;
 #if !NETSTANDARD_10 && !NETSTANDARD_11
 [assembly: Guid("A619D960-0942-47BC-B908-F940ECBFA1E1")]
 #endif
-[assembly: AssemblyVersion("1.2.*")]
-[assembly: AssemblyFileVersion("1.2.0")]
+[assembly: AssemblyVersion("1.2.0.*")]
+#pragma warning disable CS7035 // The specified version string does not conform to the recommended format - major.minor.build.revision
+[assembly: AssemblyFileVersion("1.2.0.*")]
+#pragma warning restore CS7035 // The specified version string does not conform to the recommended format - major.minor.build.revision
 [assembly: NeutralResourcesLanguage("en")]
