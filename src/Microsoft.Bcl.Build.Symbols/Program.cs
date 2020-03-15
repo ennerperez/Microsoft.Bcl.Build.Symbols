1b9c0b812c41457ff601bcb08b8f8f0270c62aea
Enner Pérez <ennerperez@gmail.com>
2020-03-15 01:12:23 -0500

    General Update

---
+++ src/Microsoft.Bcl.Build.Symbols/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Microsoft.Bcl.Build.Symbols
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ src/Microsoft.Bcl.Build.Symbols/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Microsoft.Bcl.Build.Symbols")]
+[assembly: AssemblyDescription("Provides compile time symbols for .NET Framework.")]
+[assembly: AssemblyCompany("Enner Pérez")]
+[assembly: AssemblyProduct("Microsoft.Bcl.Build.Symbols")]
+[assembly: AssemblyCopyright("Copyright © Enner Pérez")]
+[assembly: AssemblyTrademark("Microsoft")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+#if !NETSTANDARD_10 && !NETSTANDARD_11
+[assembly: Guid("A619D960-0942-47BC-B908-F940ECBFA1E1")]
+#endif
+[assembly: AssemblyVersion("1.2.*")]
+[assembly: AssemblyFileVersion("1.2.0")]
+[assembly: NeutralResourcesLanguage("en")]
